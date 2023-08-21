1f184e310cc87209f41a687644f6e95ecc1406e3
kirichenkodv <kirichenko.dmitriy1977@gmail.com>
2023-08-21 22:06:59 +1000

    add 4.7.15

--- Module_4.7.14/pet.py
+++ Module_4.7.14/pet.py
@@ -1,14 +1,35 @@
 class Pet:
+    names = []
 
-    def __init__(self. name):
+    def __init__(self, name):
         self.name = name
+        self.names.append(name)
+
+    @classmethod
+    def first_pet(cls):
+        return None if not cls.names else cls.names[0]
+    
+    @classmethod
+    def last_pet(cls):
+        return None if not cls.names else cls.names[-1]
+    
+    @classmethod
+    def num_of_pets(cls):
+        return len(cls.names)
+    
+
+    
+    
 
 
 
 
 
 
-# TEST_1:
-print(Pet.first_pet())
-print(Pet.last_pet())
+pet1 = Pet('Ratchet')
+pet2 = Pet('Clank')
+pet3 = Pet('Rivet')
+
+print(Pet.first_pet().name)
+print(Pet.last_pet().name)
 print(Pet.num_of_pets())
